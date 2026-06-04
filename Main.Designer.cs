358584f48ee62bf2a382ab98d8716dfa77d0efb1
Nikk <niko.rehu@outlook.com>
2026-06-04 13:06:05 +0300

    Added recents list (Not functional yet)

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             listBox_News = new ListBox();
+            listBox_Recents = new ListBox();
+            label_Headlines = new Label();
+            label_RecentlyRead = new Label();
             SuspendLayout();
             // 
             // listBox_News
@@ -36,26 +39,64 @@
             listBox_News.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             listBox_News.BorderStyle = BorderStyle.FixedSingle;
             listBox_News.FormattingEnabled = true;
-            listBox_News.Location = new Point(12, 12);
+            listBox_News.Location = new Point(12, 37);
             listBox_News.Name = "listBox_News";
-            listBox_News.Size = new Size(366, 422);
+            listBox_News.Size = new Size(366, 242);
             listBox_News.TabIndex = 2;
             listBox_News.MouseClick += listBox_News_MouseClick;
+            // 
+            // listBox_Recents
+            // 
+            listBox_Recents.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            listBox_Recents.BorderStyle = BorderStyle.FixedSingle;
+            listBox_Recents.FormattingEnabled = true;
+            listBox_Recents.Location = new Point(12, 321);
+            listBox_Recents.Name = "listBox_Recents";
+            listBox_Recents.Size = new Size(366, 212);
+            listBox_Recents.TabIndex = 3;
+            listBox_Recents.MouseClick += listBox_Recents_MouseClick;
+            // 
+            // label_Headlines
+            // 
+            label_Headlines.AutoSize = true;
+            label_Headlines.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label_Headlines.Location = new Point(12, 9);
+            label_Headlines.Name = "label_Headlines";
+            label_Headlines.Size = new Size(99, 25);
+            label_Headlines.TabIndex = 4;
+            label_Headlines.Text = "Headlines";
+            // 
+            // label_RecentlyRead
+            // 
+            label_RecentlyRead.AutoSize = true;
+            label_RecentlyRead.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label_RecentlyRead.Location = new Point(12, 293);
+            label_RecentlyRead.Name = "label_RecentlyRead";
+            label_RecentlyRead.Size = new Size(136, 25);
+            label_RecentlyRead.TabIndex = 5;
+            label_RecentlyRead.Text = "Recently Read";
             // 
             // Main
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(390, 450);
+            ClientSize = new Size(390, 545);
+            Controls.Add(label_RecentlyRead);
+            Controls.Add(label_Headlines);
+            Controls.Add(listBox_Recents);
             Controls.Add(listBox_News);
             MaximizeBox = false;
             MinimumSize = new Size(406, 489);
             Name = "Main";
             Text = "YleRSS - News";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
         private ListBox listBox_News;
+        private ListBox listBox_Recents;
+        private Label label_Headlines;
+        private Label label_RecentlyRead;
     }
 }
--- Main.cs
+++ Main.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Reflection;
 using System.Xml;
 using System.Xml.Linq;
 
@@ -6,17 +7,27 @@ namespace YleRSS
 {
     public partial class Main : Form
     {
+        List<RecentNewsItem> recents = new List<RecentNewsItem>();
+
+        string recentsFilePath = Path.Combine(
+            Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+            "recents.txt"
+        );
+
         public Main()
         {
             InitializeComponent();
             Setup();
         }
+
         public class NewsItem
         {
             public string? Title { get; set; }
             public string? Link { get; set; }
             public string? Description { get; set; }
         }
+
+        public class RecentNewsItem : NewsItem { }
 
         private async void Setup()
         {
@@ -34,13 +45,62 @@ namespace YleRSS
                 Description = x.Element("description")?.Value
             }).ToList();
 
-            listBox_News.DataSource = items;
-            listBox_News.DisplayMember = "Title";
+            this.listBox_News.DataSource = items;
+            this.listBox_News.DisplayMember = "Title";
+            PopulateRecents();
         }
 
         private void listBox_News_MouseClick(object sender, MouseEventArgs e)
         {
             if (listBox_News.SelectedItem is NewsItem item)
+            {
+                LogRecent(item);
+                PopulateRecents();
+                ArticleDescription descriptionWindow = new ArticleDescription(item);
+                descriptionWindow.Visible = true;
+            }
+        }
+
+        private void LogRecent(NewsItem item)
+        {
+            if (!File.Exists(recentsFilePath))
+            {
+                File.Create(recentsFilePath).Close();
+            }
+
+            string output = item.Title + "|" + item.Link + "|" + item.Description;
+            File.AppendAllText(recentsFilePath, output + "\n");
+        }
+
+        private void PopulateRecents()
+        {
+            if (!File.Exists(recentsFilePath))
+            {
+                File.Create(recentsFilePath).Close();
+                return;
+            }
+
+            string[] rawRecents = File.ReadAllLines(recentsFilePath);
+            for (int i = 0; i < rawRecents.Length; i++)
+            {
+                string[] s = rawRecents[i].Split("|");
+                RecentNewsItem r = new RecentNewsItem
+                {
+                    Title = s[0],
+                    Link = s[1],
+                    Description = s[2]
+                };
+
+                recents.Add(r);
+            }
+            this.listBox_Recents.DataSource = null;
+            this.listBox_Recents.DataSource = recents;
+            this.listBox_Recents.DisplayMember = "Title";
+        }
+
+        private void listBox_Recents_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (listBox_News.SelectedItem is RecentNewsItem item)
             {
                 ArticleDescription descriptionWindow = new ArticleDescription(item);
                 descriptionWindow.Visible = true;
